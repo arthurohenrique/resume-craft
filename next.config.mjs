f8d6803fa5bd344f038272f5e2545aaeabba0d60
Arthur Henrique <arthurhlsouza@gmail.com>
2024-12-13 11:11:50 +0000

    modifica next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,42 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    basePath: '/resume-craft',
-    webpack(config) {
-        const fileLoaderRule = config.module.rules.find((rule) =>
-          rule.test?.test?.(".svg")
-        );
-    
-        config.module.rules.push(
-          {
-            ...fileLoaderRule,
-            test: /\.svg$/i,
-            resourceQuery: /url/,
-          },
-          {
-            test: /\.svg$/i,
-            issuer: fileLoaderRule.issuer,
-            resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
-            use: ["@svgr/webpack"],
-          }
-        );
-    
-        fileLoaderRule.exclude = /\.svg$/i;
-    
-        return config;
+  reactStrictMode: false,
+  basePath: '/resume-craft',
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg")
+    );
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
       },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: ["@svgr/webpack"],
+      }
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
+  
+  outputFileTracing: false, // Desativa a inclusão automática de arquivos de dependência no build
+  compress: true, // Ativa a compressão Gzip/Brotli
+  eslint: {
+    ignoreDuringBuilds: true // Ignora erros de ESLint no build
+  },
+  typescript: {
+    ignoreBuildErrors: true // Ignora erros de TypeScript no build
+  },
+  experimental: {
+    outputStandalone: true // Gera uma build mais enxuta, excluindo arquivos desnecessários
+  }
 };
 
 export default nextConfig;
